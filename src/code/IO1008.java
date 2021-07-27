fa5891696c2024ee58a21b9b3473dbddbb912e28
beom1843 <beom1843@gmail.com>
2021-07-27 14:35:06 +0900

    feat. 1008, 2557

---
+++ src/code/IO1008.java
@@ -0,0 +1,25 @@
+package code;
+
+import java.util.Scanner;
+
+public class IO1008 {
+
+	public static void main(String[] args) {
+		//문제: 두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+		//입력: 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+		//출력: 첫째 줄에 A/B를 출력한다. 실제 정답과 출력값의 절대오차 또는 상대오차가 10-9 이하이면 정답이다.
+		
+		
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		int b = s.nextInt();
+		
+		double r = (double) a/b;
+		System.out.println(r);
+		
+		s.close();
+		
+		
+		
+	}
+}
---
+++ src/code/IO2557.java
@@ -0,0 +1,8 @@
+package code;
+
+public class IO2557 {
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+	}
+}
